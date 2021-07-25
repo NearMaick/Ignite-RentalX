f748861a1be30430823f8cc3ac44c372285ec0e3
NearMaick <maick_a_s@msn.com>
2021-07-25 14:16:08 -0300

    feat(server): specification repository

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,13 @@
+import { Specification } from '../model/Specification';
+
+interface ISpecificationRepositoryDTO {
+  name: string;
+  description: string;
+}
+
+interface ISpecificationsRepository {
+  create({ description, name }: ISpecificationRepositoryDTO): void;
+  findByName(name: string): Specification;
+}
+
+export { ISpecificationsRepository, ISpecificationRepositoryDTO };
