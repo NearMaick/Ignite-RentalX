b233cd9e46faedeac0dfbf298a231005d3864176
NearMaick <maick_a_s@msn.com>
2021-07-25 17:11:21 -0300

    fix(server): singleton pattern

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -2,9 +2,9 @@ import { CategoriesRepository } from '../../repositories/CategoriesRepository';
 import { CreateCategoryController } from './CreateCategoryController';
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
-const categoryRepository = new CategoriesRepository();
+const categoriesRepository = CategoriesRepository.getInstance();
 
-const createCategoryUseCase = new CreateCategoryUseCase(categoryRepository);
+const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
 
 const createCategoryController = new CreateCategoryController(
   createCategoryUseCase
