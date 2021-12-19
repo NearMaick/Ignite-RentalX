85ae6d9da8f64979470abe887303521a4da11e1a
Maick Souza <maick_a_s@msn.com>
2021-12-19 09:15:40 -0300

    feat(server): especification migration

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -1,4 +1,4 @@
-import { Specification } from "../model/Specification";
+import { Specification } from "../entities/Specification";
 
 interface ISpecificationRepositoryDTO {
   name: string;
@@ -6,8 +6,8 @@ interface ISpecificationRepositoryDTO {
 }
 
 interface ISpecificationsRepository {
-  create({ description, name }: ISpecificationRepositoryDTO): void;
-  findByName(name: string): Specification;
+  create({ description, name }: ISpecificationRepositoryDTO): Promise<void>;
+  findByName(name: string): Promise<Specification>;
 }
 
 export { ISpecificationsRepository, ISpecificationRepositoryDTO };
