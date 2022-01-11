c65725de5f60de8dcebf653efaca7b30bdc51585
Maick Souza <maick_a_s@msn.com>
2022-01-11 18:31:04 -0300

    feat(server): cars Entity

--- src/modules/cars/useCases/createCar/CreateCarUseCase.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -1,10 +1,16 @@
+import { inject, injectable } from "tsyringe";
+
 import { ICreateCarDTO } from "@modules/cars/dtos/ICreateCarDTO";
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { AppError } from "@shared/errors/AppError";
 
+@injectable()
 export class CreateCarUseCase {
-  constructor(private carsRepository: ICarsRepository) {}
+  constructor(
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository
+  ) {}
 
   async execute({
     name,
