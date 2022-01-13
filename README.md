c04709c03c4f0037d0b449c552257555702c0349
Maick Souza <maick_a_s@msn.com>
2022-01-13 10:20:33 -0300

    test(server): CreateCarSpecification #1

--- README.md
+++ README.md
@@ -27,8 +27,6 @@ O usuário não precisa estar logado no sistema
 
 **RF** => Requisitos Funcionais
 Deve ser possível cadastrar uma especificação para um carro
-Deve ser possível listar todas as especificações
-Deve ser possível listar todos os carros
 
 **RN** => Regra de Negócio
 Não deve ser possível cadastrar uma especificação para uma carro não cadastrado
---
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -0,0 +1,63 @@
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
+
+interface IRequest {
+  car_id: string;
+  specifications_id: string[];
+}
+
+export class CreateCarSpecificationUseCase {
+  constructor(private carsRepository: ICarsRepository) {}
+
+  async execute({ car_id, specifications_id }: IRequest) {
+    const carsExists = await this.carsRepository.findById(car_id);
+
+    if (!carsExists) {
+      throw new AppError("Car does not exists!");
+    }
+  }
+}
+
+let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe("Create Car Specification", () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    createCarSpecificationUseCase = new CreateCarSpecificationUseCase(
+      carsRepositoryInMemory
+    );
+  });
+
+  it("should not be able to add a new specification to a non-existent car", async () => {
+    expect(async () => {
+      const car_id = "1234";
+      const specifications_id = ["54321"];
+
+      await createCarSpecificationUseCase.execute({
+        car_id,
+        specifications_id,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should be able to add a new specification to the car", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Name Car",
+      description: "Description Car",
+      daily_rate: 100,
+      license_plate: "ABC-1234",
+      fine_amount: 60,
+      brand: "Brand",
+      category_id: "category",
+    });
+
+    const specifications_id = ["54321"];
+
+    await createCarSpecificationUseCase.execute({
+      car_id: car.id,
+      specifications_id,
+    });
+  });
+});
