c6cf13ca2055a7509e319b075726f363c4e8d897
Maick Souza <maick_a_s@msn.com>
2022-01-21 08:03:53 -0300

    feat(server): List Rentals by user #1

--- src/modules/rentals/repositories/RentalsRepository.ts
+++ src/modules/rentals/repositories/RentalsRepository.ts
@@ -51,4 +51,12 @@ export class RentalsRepository implements IRentalsRepository {
     const rental = await this.repository.findOne(id);
     return rental;
   }
+
+  async findByUser(user_id: string): Promise<Rental[]> {
+    const rentals = await this.repository.find({
+      user_id,
+    });
+
+    return rentals;
+  }
 }
